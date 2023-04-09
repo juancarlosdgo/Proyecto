94826e8ec2b5bc00de860c6e9c25bac8c9c515c2
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-04-09 00:58:29 -0500

    Delete Ejercicios varios directory

--- Ejercicios varios/Ejercicio con el teorema de Pitágoras.py
+++
@@ -1,5 +0,0 @@
-from math import sqrt
-a=float(input("Ingrese el valor del cateto a: "))
-b=float(input("Ingrese el valor del cateto b: "))
-h=sqrt(a**2+b**2)
-print("La hipotenusa es:", h)
--- Ejercicios varios/Fórmula cuadrática.py
+++
@@ -1,6 +0,0 @@
-#La fórmula cuadrática consiste en ax**2+bx+c=0
-a=int(input("Ingrese el valor de a: "))
-b=int(input("Ingrese el valor de b: "))
-c=int(input("Ingrese el valor de c: "))
-d=(b*b)-4*a*c
-
