4506e9b10dd7f4871018e31f50ff46daf2fda74d
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-04-09 00:57:35 -0500

    Delete Proyecto sqlite y python directory

--- Proyecto sqlite y python/xd.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn=sqlite3.connect("proyectosena.db")
-cursor=conn.cursor()
-tabla=input("Ingrese el nombre de la tabla")
-cursor.execute(f"PRAGMA table_info ({tabla})")
-columnas=cursor.fetchall()
-print(len(columnas))
-conn.close()
