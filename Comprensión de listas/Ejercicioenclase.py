a6f2ce2d77b8fa77c5645434385b2b9994e4c605
Catalina <116193295+Catalina236@users.noreply.github.com>
2023-04-09 00:57:00 -0500

    Delete Comprensión de listas directory

--- Comprensión de listas/Ejercicioenclase.py
+++
@@ -1,8 +0,0 @@
-import random
-tam=int(input("Cuántos números: "))
-vector=[]
-for i in range(tam):
-    vector.append(round(random.random()*100))
-    print(vector)
-suma=tam+vector.append
-print("La suma es de: ", suma)
